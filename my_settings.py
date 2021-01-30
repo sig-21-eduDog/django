7ae03a6df8ca85240b8caf356f9766ebf4234549
박유경 <pyk0514@kumoh.ac.kr>
2021-01-30 18:21:39 +0900

    DB 연동 성공

---
+++ my_settings.py
@@ -0,0 +1,11 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME' : 'rljg_schema',
+        'USER': 'RLJG_MANAGER',
+        'PASSWORD': 'q1W2e3R4',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
+SECRET_KEY = ''
--- qna/models.py
+++ qna/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Data(models.Model):
+    id = models.AutoField(db_column='ID', primary_key=True)  # Field name made lowercase.
+    source = models.CharField(db_column='SOURCE', max_length=100)  # Field name made lowercase.
+    cid = models.IntegerField(db_column='CID')  # Field name made lowercase.
+    categoryid = models.IntegerField(db_column='CATEGORYID')  # Field name made lowercase.
+    docid = models.IntegerField(db_column='DOCID')  # Field name made lowercase.
+    title = models.CharField(db_column='TITLE', max_length=150)  # Field name made lowercase.
+    main = models.CharField(db_column='MAIN', max_length=10000)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'data'
